4e76ac034b5724317722be496dad79a90f7952e1
Nick Harper <bignickharper@gmail.com>
2020-12-19 19:40:20 -0800

    got keys sorted out

---
+++ HTML/keys.js
@@ -0,0 +1,403 @@
+
+
+
+
+const cMajor = {
+    chords: [
+        "C",
+        "Dm",
+        "Em",
+        "F",
+        "G",
+        "Am",
+        "Bdim"
+    ],
+    scaleGraphic: "./images/cMajor.svg",
+    relative: "Am"
+}
+
+const aMinor = {
+    chords: [
+        "Am",
+        "Bdim",
+        "C",
+        "Dm",
+        "Em",
+        "F",
+        "G"
+    ],
+    scaleGraphic: "./images/aMinor.svg",
+    relative: "C"
+}
+
+const gMajor = {
+    chords: [
+        "G",
+        "Am",
+        "Bm",
+        "C",
+        "D",
+        "Em",
+        "F♯dim"
+    ],
+    scaleGraphic: "./images/gMajor.svg",
+    relative: "Em"
+}
+
+const eMinor = {
+    chords: [
+        "Em",
+        "F♯dim",
+        "G",
+        "Am",
+        "Bm",
+        "C",
+        "D"
+    ],
+    scaleGraphic: "./images/eMinor.svg",
+    relative: "G"
+}
+
+const dMajor = {
+    chords: [
+        "D",
+        "Em",
+        "F♯m",
+        "G",
+        "A",
+        "Bm",
+        "C♯dim"
+    ],
+    scaleGraphic: "./images/dMajor.svg",
+    relative: "Bm"
+}
+
+const bMinor = {
+    chords: [
+        "Bm",
+        "C♯dim",
+        "D",
+        "Em",
+        "F♯m",
+        "G",
+        "A"
+    ],
+    scaleGraphic: "./images/bMinor.svg",
+    relative: "D"
+}
+
+const aMajor = {
+    chords: [
+        "A",
+        "Bm",
+        "C♯m",
+        "D",
+        "E",
+        "F♯m",
+        "G♯dim"
+    ],
+    scaleGraphic: "./images/aMajor.svg",
+    relative: "F♯m"
+}
+
+const fSharpMinor = {
+    chords: [
+        "F♯m",
+        "G♯dim",
+        "A",
+        "Bm",
+        "C♯m",
+        "D",
+        "E"
+    ],
+    scaleGraphic: "./images/fSharpMinor.svg",
+    relative: "A"
+}
+
+const eMajor = {
+    chords: [
+        "E",
+        "F♯m",
+        "G♯m",
+        "A",
+        "B",
+        "C♯m",
+        "D♯dim"
+    ],
+    scaleGraphic: "./images/eMajor.svg",
+    relative: "C♯m"
+}
+
+const cSharpMinor = {
+    chords: [
+        "C♯m",
+        "D♯dim",
+        "E",
+        "F♯m",
+        "G♯m",
+        "A",
+        "B"
+    ],
+    scaleGraphic: "./images/cSharpMinor.svg",
+    relative: "E"
+}
+
+const bMajor = {
+    chords: [
+        "B",
+        "C♯m",
+        "D♯m",
+        "E",
+        "F♯",
+        "G♯m",
+        "A♯dim"
+    ],
+    scaleGraphic: "./images/bMajor.svg",
+    relative: "G♯m"
+}
+
+const gSharpMinor = {
+    chords: [
+        "G♯m",
+        "A♯dim",
+        "B",
+        "C♯m",
+        "D♯m",
+        "E",
+        "F♯"
+    ],
+    scaleGraphic: "./images/gSharpMinor.svg",
+    relative: "B"
+}
+
+const fSharpMajor = {
+    chords: [
+        "F♯",
+        "G♯m",
+        "A♯m",
+        "B",
+        "C♯",
+        "D♯m",
+        "E♯dim"
+    ],
+    scaleGraphic: "./images/fSharpMajor.svg",
+    relative: "D♯m"
+}
+
+const dSharpMinor = {
+    chords: [
+        "D♯m",
+        "E♯dim",
+        "F♯",
+        "G♯m",
+        "A♯m",
+        "B",
+        "C♯"
+    ],
+    scaleGraphic: "./images/dSharpMinor.svg",
+    relative: "F♯"
+}
+
+const gFlatMajor = {
+    chords: [
+        "G♭",
+        "A♭m",
+        "B♭m",
+        "C♭",
+        "D♭",
+        "E♭m",
+        "Fdim"
+    ],
+    scaleGraphic: "./images/gFlatMajor.svg",
+    relative: "E♭m"
+}
+
+const eFlatMinor = {
+    chords: [
+        "E♭m",
+        "Fdim",
+        "G♭",
+        "A♭m",
+        "B♭m",
+        "C♭",
+        "D♭"
+    ],
+    scaleGraphic: "./images/eFlatMinor.svg",
+    relative: "G♭"
+}
+
+const dFlatMajor = {
+    chords: [
+        "D♭",
+        "E♭m",
+        "Fm",
+        "G♭",
+        "A♭",
+        "B♭m",
+        "Cdim"
+    ],
+    scaleGraphic: "./images/dFlatMajor.svg",
+    relative: "B♭m"
+}
+
+const bFlatMinor = {
+    chords: [
+        "B♭m",
+        "Cdim",
+        "D♭",
+        "E♭m",
+        "Fm",
+        "G♭",
+        "A♭"
+    ],
+    scaleGraphic: "./images/bFlatMinor.svg",
+    relative: "D♭"
+}
+
+const aFlatMajor = {
+    chords: [
+        "A♭",
+        "B♭m",
+        "Cm",
+        "D♭",
+        "E♭",
+        "Fm",
+        "Gdim"
+    ],
+    scaleGraphic: "./images/aFlatMajor.svg",
+    relative: "Fm"
+}
+
+const fMinor = {
+    chords: [
+        "Fm",
+        "Gdim",
+        "A♭",
+        "B♭m",
+        "Cm",
+        "D♭",
+        "E♭"
+    ],
+    scaleGraphic: "./images/fMinor.svg",
+    relative: "A♭"
+}
+
+const eFlatMajor = {
+    chords: [
+        "E♭",
+        "Fm",
+        "Gm",
+        "A♭",
+        "B♭",
+        "Cm",
+        "Ddim"
+    ],
+    scaleGraphic: "./images/eFlatMajor.svg",
+    relative: "Cm"
+}
+
+const cMinor = {
+    chords: [
+        "Cm",
+        "Ddim",
+        "E♭",
+        "Fm",
+        "Gm",
+        "A♭",
+        "B♭"
+    ],
+    scaleGraphic: "./images/cMinor.svg",
+    relative: "E♭"
+}
+
+const bFlatMajor = {
+    chords: [
+        "B♭",
+        "Cm",
+        "Dm",
+        "E♭",
+        "F",
+        "Gm",
+        "Adim"
+    ],
+    scaleGraphic: "./images/bFlatMajor.svg",
+    relative: "Gm"
+}
+
+const gMinor = {
+    chords: [
+        "Gm",
+        "Adim",
+        "B♭",
+        "Cm",
+        "Dm",
+        "E♭",
+        "F"
+    ],
+    scaleGraphic: "./images/gMinor.svg",
+    relative: "B♭"
+}
+
+const fMajor = {
+    chords: [
+        "F",
+        "Gm",
+        "Am",
+        "B♭",
+        "C",
+        "Dm",
+        "Edim"
+    ],
+    scaleGraphic: "./images/fMajor.svg",
+    relative: "Dm"
+}
+
+const dMinor = {
+    chords: [
+        "Dm",
+        "Edim",
+        "F",
+        "Gm",
+        "Am",
+        "B♭",
+        "C"
+    ],
+    scaleGraphic: "./images/dMinor.svg",
+    relative: "F"
+}
+
+function readout() {
+    let keys = [
+        cMajor,
+        aMinor,
+        gMajor,
+        eMinor,
+        dMajor,
+        bMinor,
+        aMajor,
+        fSharpMinor,
+        eMajor,
+        cSharpMinor,
+        bMajor,
+        gSharpMinor,
+        fSharpMajor,
+        dSharpMinor,
+        gFlatMajor,
+        eFlatMinor,
+        dFlatMajor,
+        bFlatMinor,
+        aFlatMajor,
+        fMinor,
+        eFlatMajor,
+        cMinor,
+        bFlatMajor,
+        gMinor,
+        fMajor,
+        dMinor
+    ]
+
+    keys.forEach(element => {
+        console.log(element.chords);
+    });
+}
+
---
+++ HTML/script.js
@@ -0,0 +1,4 @@
+
+function test(){
+    console.log(keys.cMajor.chords[1])
+}
